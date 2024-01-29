03ad04a93a2e4aac479e7c755e1012b10ce1fab8
dome385 <dominik.niestroj@stud.hshl.de>
2024-01-29 19:28:00 +0100

    genres

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -77,7 +77,7 @@ const EditMovie = () => {
           });
 
           setMovie((m) => ({
-            ...movie,
+            ...m,
             genres: checks,
             genres_array: [],
           }));
@@ -92,6 +92,42 @@ const EditMovie = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    let errors = [];
+    let required = [
+      {
+        field: movie.title,
+        name: "title",
+      },
+      {
+        field: movie.release_date,
+        name: "release_date",
+      },
+      {
+        field: movie.runtime,
+        name: "runtime",
+      },
+      {
+        field: movie.description,
+        name: "description",
+      },
+      {
+        field: movie.mpaa_rating,
+        name: "mpaa_rating",
+      },
+    ];
+
+    required.forEach(function (obj) {
+      if (obj.field === "") {
+        errors.push(obj.name);
+      }
+    });
+
+    setErrors(errors);
+
+    if (errors.length > 0) {
+      return false;
+    }
   };
 
   const handleChange = () => (event) => {
@@ -110,6 +146,21 @@ const EditMovie = () => {
     console.log("value in handle check:", event.target.value);
     console.log("checked is", event.target.checked);
     console.log("Position is", position);
+
+    let tmpArr = movie.genres;
+    tmpArr[position].checked = !tmpArr[position].checked;
+
+    let tmpIDs = movie.genres_array;
+    if (!event.target.checked) {
+      tmpIDs.splice(tmpIDs.indexOf(event.target.value));
+    } else {
+      tmpIDs.push(parseInt(event.target.value, 10));
+    }
+
+    setMovie({
+      ...movie,
+      genres_array: tmpIDs,
+    });
   };
   return (
     <div>
@@ -170,26 +221,27 @@ const EditMovie = () => {
           errorMsg={"Please enter a description"}
           errorDiv={hasError("description") ? "text-danger" : "d-none"}
         />
+
+        <hr />
+
+        <h3>Genres</h3>
+        {movie.genres && movie.genres.length > 1 && (
+          <>
+            {Array.from(movie.genres).map((g, index) => (
+              <Checkbox
+                title={g.genre}
+                name={"genre"}
+                key={index}
+                id={"genre-" + index}
+                onChange={(event) => handleCheck(event, index)}
+                value={g.id}
+                checked={movie.genres[index].checked}
+              />
+            ))}
+          </>
+        )}
+        <button className="btn btn-primary">Save</button>
       </form>
-
-      <hr />
-
-      <h3>Genres</h3>
-      {movie.genres && movie.genres.length > 1 && (
-        <>
-          {Array.from(movie.genres).map((g, index) => (
-            <Checkbox
-              title={g.genre}
-              name={"genre"}
-              key={index}
-              id={"genre-" + index}
-              onChange={(event) => handleCheck(event, index)}
-              value={g.id}
-              checked={movie.genres[index].checked}
-            />
-          ))}
-        </>
-      )}
     </div>
   );
 };
